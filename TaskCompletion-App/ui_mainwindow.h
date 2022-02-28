3bd3c47017fc47e1ef13acae56d5d8971a130e9e
RiShAbHjOsHibot <60154195+RiShAbHjOsHibot@users.noreply.github.com>
2022-02-28 06:31:23 +0530

    Basic GUI made while learning the Qt Framework
    You need to have Qt framework for this app to work. GUI, and Core dynamic libraries are mandatory .

---
+++ TaskCompletion-App/ui_mainwindow.h
@@ -0,0 +1,95 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *toolbarLayout;
+    QVBoxLayout *tasksLayout;
+    QLabel *statusLabel;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *addTaskButton;
+    QSpacerItem *verticalSpacer;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(390, 316);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        toolbarLayout = new QHBoxLayout();
+        toolbarLayout->setObjectName(QString::fromUtf8("toolbarLayout"));
+        tasksLayout = new QVBoxLayout();
+        tasksLayout->setObjectName(QString::fromUtf8("tasksLayout"));
+        statusLabel = new QLabel(centralwidget);
+        statusLabel->setObjectName(QString::fromUtf8("statusLabel"));
+
+        tasksLayout->addWidget(statusLabel);
+
+
+        toolbarLayout->addLayout(tasksLayout);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        toolbarLayout->addItem(horizontalSpacer);
+
+        addTaskButton = new QPushButton(centralwidget);
+        addTaskButton->setObjectName(QString::fromUtf8("addTaskButton"));
+
+        toolbarLayout->addWidget(addTaskButton);
+
+
+        verticalLayout->addLayout(toolbarLayout);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        MainWindow->setCentralWidget(centralwidget);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Todo", nullptr));
+        statusLabel->setText(QCoreApplication::translate("MainWindow", "Shopping List", nullptr));
+        addTaskButton->setText(QCoreApplication::translate("MainWindow", "Add Task", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
